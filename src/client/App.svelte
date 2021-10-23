08cab351c73f9e0baf0aea2d5113e7b0d3632859
Tim Schottler <tim.schottler@cliniciannexus.com>
2021-10-23 18:23:20 -0500

    back to pixels font

--- src/client/App.svelte
+++ src/client/App.svelte
@@ -78,7 +78,7 @@
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    font-family: monospace; // 'pixels', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen-Sans, Ubuntu, Cantarell, 'Helvetica Neue', sans-serif;
+    font-family: 'pixels', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen-Sans, Ubuntu, Cantarell, 'Helvetica Neue', sans-serif;
     // letter-spacing: -0.15em;
 
     textarea,
